b55815c9b9141ea2887ebd6eb242218da2471185
jeyong <joon6093@naver.com>
2024-08-18 12:24:21 +0900

    feat : ConditionalOnProperty를 사용하여 유연성을 제공하도록 구현

--- detector/src/main/java/io/jeyong/detector/config/NPlusOneDetectorConfig.java
+++ detector/src/main/java/io/jeyong/detector/config/NPlusOneDetectorConfig.java
@@ -4,11 +4,18 @@ import io.jeyong.detector.aop.NPlusOneDetectionAop;
 import io.jeyong.detector.context.LoggingContext;
 import io.jeyong.detector.interceptor.NPlusOneStatementInspector;
 import org.hibernate.cfg.AvailableSettings;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(
+        prefix = "spring.jpa.properties.hibernate",
+        name = "detector",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class NPlusOneDetectorConfig {
 
     @Bean
