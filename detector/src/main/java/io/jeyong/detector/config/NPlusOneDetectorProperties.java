a0d2423cad410c8de240143fe7d8f1bd3b4539f9
jeyong <joon6093@naver.com>
2024-08-19 02:54:43 +0900

    chore : 인코딩 문제를 고려하여 한글 주석 제거

--- detector/src/main/java/io/jeyong/detector/config/NPlusOneDetectorProperties.java
+++ detector/src/main/java/io/jeyong/detector/config/NPlusOneDetectorProperties.java
@@ -5,14 +5,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "spring.jpa.properties.hibernate.detector")
 public class NPlusOneDetectorProperties {
 
-    /**
-     * N+1 문제 감지 기능 활성화 여부. 기본값은 false.
-     */
     private boolean enabled = false;
 
-    /**
-     * 쿼리 실행 횟수 기준 설정. 기본값은 2.
-     */
     private int threshold = 2;
 
     public boolean isEnabled() {
