3730ad57f9d26b0bcce5222201c5354fad8242df
jeyong <joon6093@naver.com>
2024-08-18 12:35:54 +0900

    refactror : NPlusOneStatementInspector의 메서드 분리 및 상수화로 가동성 향상

--- detector/src/main/java/io/jeyong/detector/interceptor/NPlusOneStatementInspector.java
+++ detector/src/main/java/io/jeyong/detector/interceptor/NPlusOneStatementInspector.java
@@ -6,6 +6,8 @@ import org.hibernate.resource.jdbc.spi.StatementInspector;
 
 public final class NPlusOneStatementInspector implements StatementInspector {
 
+    private static final String SELECT_KEYWORD = "select";
+
     private final LoggingContext loggingContext;
 
     public NPlusOneStatementInspector(final LoggingContext loggingContext) {
@@ -14,10 +16,14 @@ public final class NPlusOneStatementInspector implements StatementInspector {
 
     @Override
     public String inspect(final String sql) {
-        if (sql.toLowerCase().contains("select")) {
+        if (isSelectQuery(sql)) {
             final RequestLogDto logDto = loggingContext.getCurrentLoggingForm();
             logDto.addQueryOccurrence(sql);
         }
         return sql;
+    }
+
+    private boolean isSelectQuery(String sql) {
+        return sql.trim().toLowerCase().startsWith(SELECT_KEYWORD);
     }
 }
