463d1b7623ac54243bdb1bebd86bcabe0e2e6069
jeyong <joon6093@naver.com>
2024-08-28 22:35:37 +0900

    chore : 세번째 케이스에 엔드포인트의 주석을 명확하게 수정

--- test/src/main/java/io/jeyong/test/case3/controller/TeamController.java
+++ test/src/main/java/io/jeyong/test/case3/controller/TeamController.java
@@ -19,7 +19,7 @@ public class TeamController {
      * @formatter:off
      * Team과 Member는 일대다(1:N) 관계이며,
      * 모든 Team을 조회한 후 각 Team에 대해 별도의 쿼리로 Member를 조회
-     * @BatchSize 상황에서 N+1 감지기가 이를 감지하지 않는 것을 검증
+     * @BatchSize 상황에서 감지하지 않는 것을 검증
      * @formatter:on
      */
     @GetMapping
