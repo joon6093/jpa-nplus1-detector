8fb21b95b45957879d9c5e02d0a3784fa122f306
jeyong <joon6093@naver.com>
2024-09-04 03:56:20 +0900

    refactor: 쿼리 저장 역할을 명확히 하기 위해 메서드 이름 변경

---
+++ detector/src/main/java/io/jeyong/detector/context/QueryContext.java
@@ -0,0 +1,17 @@
+package io.jeyong.detector.context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+final class QueryContext {
+
+    private final Map<String, Long> queryCounts = new HashMap<>();
+
+    void incrementQueryCount(final String query) {
+        queryCounts.put(query, queryCounts.getOrDefault(query, 0L) + 1);
+    }
+
+    Map<String, Long> getQueryCounts() {
+        return Map.copyOf(queryCounts);
+    }
+}
