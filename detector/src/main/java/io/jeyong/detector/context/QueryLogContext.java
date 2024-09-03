9978f6c2511cc1e0d9a6da62c81034babd67e34a
jeyong <joon6093@naver.com>
2024-09-04 03:07:39 +0900

    refactor: 쿼리를 저장하는 역할을 명확히 드러내도록 클래스 이름 변경

---
+++ detector/src/main/java/io/jeyong/detector/context/QueryLogContext.java
@@ -0,0 +1,17 @@
+package io.jeyong.detector.context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+final class QueryLogContext {
+
+    private final Map<String, Long> queryExecutionCounts = new HashMap<>();
+
+    void incrementQueryCount(final String query) {
+        queryExecutionCounts.put(query, queryExecutionCounts.getOrDefault(query, 0L) + 1);
+    }
+
+    public Map<String, Long> getLoggedQueries() {
+        return Map.copyOf(queryExecutionCounts);
+    }
+}
