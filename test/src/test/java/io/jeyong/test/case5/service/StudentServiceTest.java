c14118c33ec3144621759f5f24c1947dbfc332f1
jeyong <118044367+joon6093@users.noreply.github.com>
2024-09-16 05:27:05 +0900

    test : ManyToMany 상황의 N+1 문제를 감지하는 것을 검증하는 테스트 코드 작성

---
+++ test/src/test/java/io/jeyong/test/case5/service/StudentServiceTest.java
@@ -0,0 +1,27 @@
+package io.jeyong.test.case5.service;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+@ExtendWith(OutputCaptureExtension.class)
+@SpringBootTest
+public class StudentServiceTest {
+
+    @Autowired
+    private StudentService studentService;
+
+    @Test
+    @DisplayName("N:N 관계에서 연관관계의 주인인 다(N)를 조회하는 상황에서 감지한다.")
+    void testFindAllStudents(CapturedOutput output) {
+        studentService.findAllStudents();
+
+        assertThat(output).contains("N+1 issue detected");
+    }
+}
