ce9ab315cbb0149ec628a6fe930c75360daa3a15
jeyong <118044367+joon6093@users.noreply.github.com>
2024-09-16 04:58:22 +0900

    chore : test 케이스에 대한 javadoc의 @formatter 위치 문제 수정

--- test/src/main/java/io/jeyong/test/case3/controller/TeamController.java
+++ test/src/main/java/io/jeyong/test/case3/controller/TeamController.java
@@ -15,13 +15,13 @@ public class TeamController {
 
     private final TeamService teamService;
 
+    // @formatter:off
     /**
-     * @formatter:off
      * Team과 Member는 일대다(1:N) 관계이며,
      * 모든 Team을 조회한 후 각 Team에 대해 별도의 쿼리로 Member를 조회
      * @BatchSize 상황에서 감지하지 않는 것을 검증
-     * @formatter:on
      */
+    // @formatter:on
     @GetMapping
     public List<Team> getAllTeams() {
         return teamService.findAllTeams();
--- test/src/main/java/io/jeyong/test/case4/controller/AddressController.java
+++ test/src/main/java/io/jeyong/test/case4/controller/AddressController.java
@@ -15,13 +15,13 @@ public class AddressController {
 
     private final AddressService addressService;
 
+    // @formatter:off
     /**
-     * @formatter:off
      * Address와 Person은 일대일(1:1) 관계
      * 모든 Address을 조회한 후 각 Address에 대해 즉시로딩으로 Person를 조회
      * 1:1 관계에서 연관관계의 주인이 아닌 일(1)을 조회하는 상황에서 감지하는 것을 검증
-     * @formatter:on
      */
+    // @formatter:on
     @GetMapping
     public List<Address> getAllAddresses() {
         return addressService.findAllAddresses();
