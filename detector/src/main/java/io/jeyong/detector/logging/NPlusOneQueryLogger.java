6a78635bfb17314f575414b7ed00fb790f0ff790
jeyong <118044367+joon6093@users.noreply.github.com>
2024-09-17 07:05:40 +0900

    feat : 사용자가 지정한 로그 레벨으로 출력하도록 구현

--- detector/src/main/java/io/jeyong/detector/logging/NPlusOneQueryLogger.java
+++ detector/src/main/java/io/jeyong/detector/logging/NPlusOneQueryLogger.java
@@ -3,23 +3,39 @@ package io.jeyong.detector.logging;
 import io.jeyong.detector.context.QueryContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 
 public final class NPlusOneQueryLogger {
 
     private static final Logger logger = LoggerFactory.getLogger(NPlusOneQueryLogger.class);
     private final int queryThreshold;
+    private final Level level;
 
-    public NPlusOneQueryLogger(final int queryThreshold) {
+    public NPlusOneQueryLogger(final int queryThreshold, final Level level) {
         this.queryThreshold = queryThreshold;
+        this.level = level;
     }
 
     public void logNPlusOneIssues() {
         QueryContextHolder.getContext().getQueryCounts().forEach((query, count) -> {
             if (count >= queryThreshold) {
-                logger.warn("N+1 issue detected: '{}' was executed {} times.", query, count);
+                logBasedOnLevel(query, count);
             }
         });
-
         QueryContextHolder.clearContext();
+    }
+
+    private void logBasedOnLevel(String query, long count) {
+        if (level == Level.WARN && logger.isWarnEnabled()) {
+            logger.warn("N+1 issue detected: '{}' was executed {} times.", query, count);
+        } else if (level == Level.TRACE && logger.isTraceEnabled()) {
+            logger.trace("N+1 issue detected: '{}' was executed {} times.", query, count);
+        } else if (level == Level.INFO && logger.isInfoEnabled()) {
+            logger.info("N+1 issue detected: '{}' was executed {} times.", query, count);
+        } else if (level == Level.DEBUG && logger.isDebugEnabled()) {
+            logger.debug("N+1 issue detected: '{}' was executed {} times.", query, count);
+        } else if (level == Level.ERROR && logger.isErrorEnabled()) {
+            logger.error("N+1 issue detected: '{}' was executed {} times.", query, count);
+        }
     }
 }
