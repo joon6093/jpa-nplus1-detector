5ebc615ad0f64800a5effd0740dc9216555a48cd
jeyong <joon6093@naver.com>
2024-09-19 07:25:07 +0900

    test : @NPlusOneTest가 로그 설정에서 API와 비지니스 로직에서 검증하는 테스트 코드 작성

--- test/src/test/java/io/jeyong/test/mode/AnnotationLoggingModeTest.java
+++ test/src/test/java/io/jeyong/test/mode/AnnotationLoggingModeTest.java
@@ -1,28 +1,71 @@
 package io.jeyong.test.mode;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import io.jeyong.detector.test.NPlusOneTest;
+import io.jeyong.test.case2.service.ProductService;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.AssertionsForInterfaceTypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
 @NPlusOneTest(threshold = 3, level = Level.DEBUG, mode = NPlusOneTest.Mode.LOGGING)
+@ExtendWith(OutputCaptureExtension.class)
+@SpringBootTest(
+        webEnvironment = RANDOM_PORT,
+        properties = {
+                "logging.level.io.jeyong=debug",
+        })
 class AnnotationLoggingModeTest {
 
     @Autowired
-    private ApplicationContext context;
+    private ApplicationContext applicationContext;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ProductService productService;
 
     @Test
-    @DisplayName("LOGGING 모드의 설정이 제대로 동작한다.")
-    void testNPlusOneTestLoggingMode() {
+    @DisplayName("LOGGING 모드의 설정이 동작한다.")
+    void testLoggingModeConfiguration() {
         assertThat(System.getProperty("spring.jpa.properties.hibernate.detector.threshold")).isEqualTo("3");
         assertThat(System.getProperty("spring.jpa.properties.hibernate.detector.level")).isEqualTo("DEBUG");
 
-        assertThat(context.containsBean("nPlusOneQueryLogger")).isTrue();
+        assertThat(applicationContext.containsBean("nPlusOneQueryLogger")).isTrue();
+    }
+
+    @Test
+    @DisplayName("API 호출에서 LOGGING 모드가 동작한다.")
+    void testLoggingModeInApiCall(CapturedOutput output) {
+        String url = "http://localhost:" + port + "/api/products";
+        ResponseEntity<List> response = restTemplate.getForEntity(url, List.class);
+
+        Assertions.assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+        Assertions.assertThat(output).contains("N+1 issue detected");
+    }
+
+    @Test
+    @DisplayName("Business Logic 호출에서 LOGGING 모드가 동작한다.")
+    void testLoggingModeInBusinessLogicCall(CapturedOutput output) {
+        productService.findAllProducts();
+
+        AssertionsForInterfaceTypes.assertThat(output).contains("N+1 issue detected");
     }
 }
