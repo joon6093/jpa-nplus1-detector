0bd9615a3804c04fd59d0dc6a6341d575a5858f8
jeyong <118044367+joon6093@users.noreply.github.com>
2024-09-19 14:41:46 +0900

    fix : ConfigurableEnvironment를 이용하여 환경 변수의 우선순위 문제 해결

--- detector/src/main/java/io/jeyong/detector/test/NplusOneTestImportSelector.java
+++ detector/src/main/java/io/jeyong/detector/test/NplusOneTestImportSelector.java
@@ -2,27 +2,45 @@ package io.jeyong.detector.test;
 
 import io.jeyong.detector.config.NPlusOneDetectorConfig;
 import java.util.Map;
+import java.util.Objects;
 import org.slf4j.event.Level;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.lang.NonNull;
 
-public class NplusOneTestImportSelector implements ImportSelector {
+public final class NplusOneTestImportSelector implements ImportSelector, EnvironmentAware {
+
+    private ConfigurableEnvironment environment;
+
+    @Override
+    public void setEnvironment(@NonNull final Environment environment) {
+        this.environment = (ConfigurableEnvironment) environment;
+    }
 
     @NonNull
     @Override
-    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        Map<String, Object> attributes = importingClassMetadata
-                .getAnnotationAttributes(NPlusOneTest.class.getName());
+    public String[] selectImports(final AnnotationMetadata importingClassMetadata) {
+        final Map<String, Object> attributes = Objects.requireNonNull(
+                importingClassMetadata.getAnnotationAttributes(NPlusOneTest.class.getName()),
+                "Attributes for @NPlusOneTest cannot be null"
+        );
 
-        assert attributes != null;
-        int threshold = (int) attributes.get("threshold");
-        Level logLevel = (Level) attributes.get("level");
-        NPlusOneTest.Mode mode = (NPlusOneTest.Mode) attributes.get("mode");
+        final int threshold = (int) attributes.get("threshold");
+        final Level logLevel = (Level) attributes.get("level");
+        final NPlusOneTest.Mode mode = (NPlusOneTest.Mode) attributes.get("mode");
 
-        System.setProperty("spring.jpa.properties.hibernate.detector.enabled", "true");
-        System.setProperty("spring.jpa.properties.hibernate.detector.threshold", String.valueOf(threshold));
-        System.setProperty("spring.jpa.properties.hibernate.detector.level", logLevel.toString());
+        final Map<String, Object> propertyMap = Map.of(
+                "spring.jpa.properties.hibernate.detector.enabled", "true",
+                "spring.jpa.properties.hibernate.detector.threshold", String.valueOf(threshold),
+                "spring.jpa.properties.hibernate.detector.level", logLevel.toString()
+        );
+
+        environment.getPropertySources().addFirst(new MapPropertySource("nPlusOneDetectorProperties", propertyMap));
+
         return new String[]{
                 mode == NPlusOneTest.Mode.LOGGING
                         ? NPlusOneDetectorConfig.class.getName()
