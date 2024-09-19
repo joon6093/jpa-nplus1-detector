79d670b7184097d937a4e1241c9fb93ba99cf10f
jeyong <joon6093@naver.com>
2024-09-19 19:52:24 +0900

    refactor : ImportSelector에서 모드 별로 다르게 설정 하도록 변경

--- detector/src/main/java/io/jeyong/detector/test/NplusOneTestImportSelector.java
+++ detector/src/main/java/io/jeyong/detector/test/NplusOneTestImportSelector.java
@@ -1,6 +1,8 @@
 package io.jeyong.detector.test;
 
-import io.jeyong.detector.config.NPlusOneDetectorConfig;
+import io.jeyong.detector.config.NPlusOneDetectorExceptionConfig;
+import io.jeyong.detector.config.NPlusOneDetectorLoggingConfig;
+import io.jeyong.detector.test.NPlusOneTest.Mode;
 import java.util.Map;
 import java.util.Objects;
 import org.slf4j.event.Level;
@@ -29,22 +31,40 @@ public final class NplusOneTestImportSelector implements ImportSelector, Environ
                 "Attributes for @NPlusOneTest cannot be null"
         );
 
+        final Mode mode = (Mode) attributes.get("mode");
+
+        if (mode == Mode.LOGGING) {
+            configureForLoggingMode(attributes);
+            return new String[]{NPlusOneDetectorLoggingConfig.class.getName()};
+        } else if (mode == Mode.EXCEPTION) {
+            configureForExceptionMode(attributes);
+            return new String[]{NPlusOneDetectorExceptionConfig.class.getName()};
+        } else {
+            throw new IllegalArgumentException("Invalid mode provided for @NPlusOneTest: " + mode);
+        }
+    }
+
+    private void configureForLoggingMode(final Map<String, Object> attributes) {
         final int threshold = (int) attributes.get("threshold");
-        final Level logLevel = (Level) attributes.get("level");
-        final NPlusOneTest.Mode mode = (NPlusOneTest.Mode) attributes.get("mode");
+        final Level level = (Level) attributes.get("level");
 
         final Map<String, Object> propertyMap = Map.of(
                 "spring.jpa.properties.hibernate.detector.enabled", "true",
                 "spring.jpa.properties.hibernate.detector.threshold", String.valueOf(threshold),
-                "spring.jpa.properties.hibernate.detector.level", logLevel.toString()
+                "spring.jpa.properties.hibernate.detector.level", level.toString()
         );
 
-        environment.getPropertySources().addFirst(new MapPropertySource("nPlusOneDetectorProperties", propertyMap));
+        environment.getPropertySources().addFirst(new MapPropertySource("DetectorProperties", propertyMap));
+    }
 
-        return new String[]{
-                mode == NPlusOneTest.Mode.LOGGING
-                        ? NPlusOneDetectorConfig.class.getName()
-                        : NPlusOneTestConfig.class.getName()
-        };
+    private void configureForExceptionMode(final Map<String, Object> attributes) {
+        final int threshold = (int) attributes.get("threshold");
+
+        final Map<String, Object> propertyMap = Map.of(
+                "spring.jpa.properties.hibernate.detector.enabled", "false",
+                "spring.jpa.properties.hibernate.detector.threshold", String.valueOf(threshold)
+        );
+
+        environment.getPropertySources().addFirst(new MapPropertySource("DetectorProperties", propertyMap));
     }
 }
