852e048a38030dd262ceb5f5024bf1664ff80202
jeyong <joon6093@naver.com>
2025-04-21 01:57:44 +0900

    chore: @Configuration 클래스명을 명확하게 수정

--- detector/src/main/java/io/jeyong/detector/config/NPlusOneDetectorBaseConfiguration.java
+++ detector/src/main/java/io/jeyong/detector/config/NPlusOneDetectorBaseConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-public class NPlusOneDetectorBaseConfig {
+public class NPlusOneDetectorBaseConfiguration {
 
     @Bean
     public ConnectionProxyAspect connectionProxyAspect(final NPlusOneQueryTemplate nPlusOneQueryTemplate) {
