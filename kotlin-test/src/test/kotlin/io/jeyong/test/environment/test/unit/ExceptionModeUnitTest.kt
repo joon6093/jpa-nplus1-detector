419beb55944676b35c66bf024590112d67f9fc75
jeyong <joon6093@naver.com>
2025-06-26 01:45:20 +0900

    test: kotlin 환경의 테스트 환경에 대해 동작을 검증

---
+++ kotlin-test/src/test/kotlin/io/jeyong/test/environment/test/unit/ExceptionModeUnitTest.kt
@@ -0,0 +1,28 @@
+package io.jeyong.test.environment.test.unit
+
+import io.jeyong.detector.annotation.NPlusOneTest
+import io.jeyong.test.environment.test.unit.config.TestInitDataConfig
+import io.jeyong.test.repository.ProductRepository
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Import
+
+@NPlusOneTest(mode = NPlusOneTest.Mode.EXCEPTION)
+@DataJpaTest
+@Import(TestInitDataConfig::class)
+class ExceptionModeUnitTest {
+
+    @Autowired
+    private lateinit var productRepository: ProductRepository
+
+    @Disabled("AfterEachCallback을 통해 발생하는 예외는 테스트할 수 없다.")
+    @Test
+    @DisplayName("@DataJpaTest를 이용한 Repository를 호출하는 상황에서 EXCEPTION 모드가 동작한다.")
+    fun testExceptionModeInRepositoryCall() {
+        val products = productRepository.findAll()
+        products.forEach { it.order?.orderNumber }  // N+1 문제 발생
+    }
+}
